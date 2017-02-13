25ed0982252af7720853977c150627fcc0d707fc
AlekseiSt <starikovaleksei1992@mail.ru>
2017-02-13 12:00:13 +0300

    First track create

---
+++ JaxbTest/src/RailRoadElement.java
@@ -0,0 +1,45 @@
+import com.sun.xml.internal.txw2.annotation.XmlCDATA;
+import org.dom4j.DocumentHelper;
+
+import javax.xml.bind.annotation.XmlElement;
+
+public class RailRoadElement {
+    @XmlElement
+    private String Id;
+    @XmlElement
+    private String Name;
+    @XmlElement
+    private int X;
+    @XmlElement
+    private int Y;
+    @XmlElement
+    private Label Label = new Label();
+    @XmlElement
+    private boolean PublicFlag = Constants.PUBLIC_FLAG;
+    @XmlElement
+    private boolean PresentationFlag = Constants.PRESENTATION_FLAG;
+    @XmlElement
+    private boolean ShowLabel = Constants.SHOW_LABEL;
+    @XmlElement
+    private String DrawMode = Constants.DRAW_MODE;
+
+    public RailRoadElement() {
+    }
+
+    public void setId(String id) {
+        this.Id = id;
+    }
+
+    @XmlCDATA
+    public void setName(String name) {
+        this.Name = DocumentHelper.createCDATA(name).asXML();
+    }
+
+    public void setX(int x) {
+        this.X = x;
+    }
+
+    public void setY(int y) {
+        this.Y = y;
+    }
+}
--- JaxbTest/src/RailTrack.java
+++ JaxbTest/src/RailTrack.java
@@ -9,24 +9,20 @@ import org.dom4j.DocumentHelper;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
-public class RailTrack {
-    private String Id;
-    private String Name;
-    private int X;
-    private int Y;
-    private Label Label;
-    private Points Points;
-    private boolean PublicFlag = Constants.PUBLIC_FLAG;
-    private boolean PresentationFlag = Constants.PRESENTATION_FLAG;
-    private boolean ShowLabel = Constants.SHOW_LABEL;
-    private String DrawMode = Constants.DRAW_MODE;
+public class RailTrack extends RailRoadElement {
+
     private int Z = Constants.Z;
     private long LineColor = Constants.LINE_COLOR;
     private String LineMaterial = Constants.LINE_MATERIAL;
     private int LineWidth = Constants.LINE_WIDTH;
     private String PathType = Constants.PATH_TYPE;
     private int Width = Constants.WIDTH;
+    private Points Points;
     private boolean Bidirectional = Constants.BIDIRECTIONAL;
+
+    public RailTrack() {
+        super();
+    }
 
     public void initRailTrack(String id, String name, int x, int y, Points points) {
         setId(id);
@@ -36,76 +32,7 @@ public class RailTrack {
         setPoints(points);
     }
 
-    public void setId(String id) {
-        Id = id;
-    }
-
-    // replace?
-    @XmlCDATA
-    public void setName(String name) {
-        Name = DocumentHelper.createCDATA(name).asXML();
-    }
-
-    public void setX(int x) {
-        X = x;
-    }
-
-    public void setY(int y) {
-        Y = y;
-    }
-
-
-    public void setLabel(int x, int y) {
-        Label = new Label();
-        Label.setX(x);
-        Label.setY(y);
-    }
-
     public void setPoints(Points points) {
         Points = points;
-    }
-
-    public void setPublicFlag(boolean publicFlag) {
-        PublicFlag = publicFlag;
-    }
-
-    public void setPresentationFlag(boolean presentationFlag) {
-        PresentationFlag = presentationFlag;
-    }
-
-    public void setShowLabel(boolean showLabel) {
-        ShowLabel = showLabel;
-    }
-
-    public void setDrawMode(String drawMode) {
-        DrawMode = drawMode;
-    }
-
-    public void setZ(int z) {
-        Z = z;
-    }
-
-    public void setLineColor(long lineColor) {
-        LineColor = lineColor;
-    }
-
-    public void setLineMaterial(String lineMaterial) {
-        LineMaterial = lineMaterial;
-    }
-
-    public void setLineWidth(int lineWidth) {
-        LineWidth = lineWidth;
-    }
-
-    public void setPathType(String pathType) {
-        PathType = pathType;
-    }
-
-    public void setWidth(int width) {
-        Width = width;
-    }
-
-    public void setBidirectional(boolean bidirectional) {
-        Bidirectional = bidirectional;
     }
 }
